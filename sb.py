565601d5c0c42baffe1c05e7671d0e2f3a693b1d
Austin Songer <austinvernsonger@protonmail.com>
2020-01-04 23:28:32 +0000

    Starter Changing codebase

---
+++ sb.py
@@ -0,0 +1,16 @@
+import os
+import time
+import sys
+
+
+print ("$red You don't have Root privilegies, execute the script as root.$nc);
+exit()
+print ("Checking Installation $nc");
+print ("Checking Completed [$green✓$nc] $nc ");
+time.sleep(1)
+print ("");
+print ("");
+print ("");
+
+
+
