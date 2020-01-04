e2bb8e77336d53e1223c85f96ed4f0f4646ab81a
Austin Songer <austinvernsonger@protonmail.com>
2020-01-04 23:36:34 +0000

    Starting with the easiest areas.

--- sb.py
+++ sb.py
@@ -1,13 +1,35 @@
+#!/usr/bin/python3
 import os
 import time
 import sys
 
 
+
+#Convert to python
+# if [
+#   [$EUID - ne 0]
+# ];
+# then
+if
+
+
+else
+
+
+
+
 print ("$red You don't have Root privilegies, execute the script as root.$nc);
 exit()
+fi# Installation
 print ("Checking Installation $nc");
 print ("Checking Completed [$green✓$nc] $nc ");
+
+
 time.sleep(1)
+os.system('cls||clear')
+
+
+
 print ("");
 print ("");
 print ("");
