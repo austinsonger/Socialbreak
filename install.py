3506247cc91c2efb9f8005f9feef8286607cf4ae
Austin Songer <austinvernsonger@protonmail.com>
2020-01-05 02:38:49 +0000

    Updated

--- install.py
+++ install.py
@@ -7,7 +7,9 @@ def install():
 	os.system("apt install openssl -y")
 	os.system("pip install mechanize")
 	os.system("pip install requests")
+    os.system("pip istall cookielib")
 	os.system("pip install bs4") 
+    os.system("pip install banner")
 	os.system("apt install perl -y")
 	print "\n"
 	print """Preparing"""
--- install.sh
+++ install.sh
@@ -14,22 +14,3 @@ sudo pip install bs4
 sudo pip install mechanize
 sudo pip install requests
 
-# Gmail
-if [ -d gemailhack ]; then
-        echo "Directory Gemail-Hack exists.[✔]"
-# Facebook
-if [ -d fbbrute ]; then
-        echo "Directory Facebook Bruteforce exists.[✔]"
-# Instainsane
-fi [ -d instainsane ]; then
-        echo "Directory instainsane exists.[✔]"
-        cd instainsane
-        chmod +x instainsane.sh
-        cd ..
-# TweetShell
-fi [ -d tweetshell ]; then
-        echo "Directory tweetshell  exists.[✔]"       
-        cd tweetshell
-        chmod +x tweetshell.sh
-        cd ..
-
